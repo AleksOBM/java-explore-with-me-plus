8f6c1ad9e953a6dc9888a3db2be31f19ce546c9f
Denis Panasyuk <den.dzim@yandex.ru>
2026-04-30 20:43:13 +0300

    добавлены методы в сервис

--- main-service/src/main/java/ru/practicum/ewm/service/CategoryServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/service/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.ewm.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.dao.CategoryRepository;
 import ru.practicum.ewm.dto.CategoryDto;
@@ -10,6 +11,7 @@ import ru.practicum.ewm.model.Category;
 import ru.practicum.ewm.util.error.exception.NotFoundException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,11 +33,17 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<CategoryDto> findAll(Integer from, Integer size) {
-        return List.of();
+        int page = from / size;
+
+        return categoryRepository.findAll(PageRequest.of(page, size)).stream()
+                .map(CategoryMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CategoryDto findById(Long catId) {
-        return null;
+        Category category = findEntityById(catId);
+
+        return CategoryMapper.toDto(category);
     }
 }
